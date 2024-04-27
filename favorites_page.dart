4dc47a9d82d107842f9974b1748a9321e010a110
j-e-ff <118487175+j-e-ff@users.noreply.github.com>
2024-04-27 15:26:06 -0700

    Update favorites_page.dart

--- favorites_page.dart
+++ favorites_page.dart
@@ -1,19 +1,63 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:on_audio_query/on_audio_query.dart';
+import '../Components/playlistModel.dart';
+import '../Components/songProvider.dart'; // Import your SongProvider class
 
-class FavoritesPage extends StatefulWidget {
-  @override
-  State<FavoritesPage> createState() => _FavoritesPageState();
-}
+class FavoritesPage extends StatelessWidget {
+  final void Function(List<SongModel>, int) onSongSelected;
 
-class _FavoritesPageState extends State<FavoritesPage> {
+  const FavoritesPage({
+    Key? key,
+    required this.onSongSelected,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-        child: Text(
-          'Favorites',
-          style: TextStyle(fontSize: 50),
-        )
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Favorites'),
+      ),
+      body: Consumer<SongProvider>(
+        builder: (context, songProvider, _) {
+          Playlist? favoritesPlaylist = songProvider.getFavoritesPlaylist();
+          if (favoritesPlaylist != null) {
+            List<String> favoriteSongIds = favoritesPlaylist.songs;
+            List<SongModel> favoriteSongs = songProvider.songs.where((song) => favoriteSongIds.contains(song.id.toString())).toList();
+            return ListView.builder(
+              itemCount: favoriteSongs.length + 1, // Add 1 for the padding
+              itemBuilder: (context, index) {
+                if (index == favoriteSongs.length) {
+                  // Return a container with padding as the last item
+                  return SizedBox(height: 70.0); // Adjust the height as needed
+                } else {
+                  final song = favoriteSongs[index];
+                  return ListTile(
+                    leading: QueryArtworkWidget(
+                      id: song.id,
+                      type: ArtworkType.AUDIO,
+                      artworkBorder: BorderRadius.circular(10.0),
+                      artworkQuality: FilterQuality.high,
+                      artworkWidth: 50,
+                      artworkHeight: 50,
+                    ),
+                    title: Text(song.title),
+                    subtitle: Text(song.artist ?? 'Unknown Artist'),
+                    onTap: () {
+                      // Call the callback function with the selected song
+                      onSongSelected(favoriteSongs, index);
+                    },
+                  );
+                }
+              },
+            );
+          } else {
+            return Center(
+              child: Text('Favorites playlist not found'),
+            );
+          }
+        },
+      ),
     );
   }
 }
